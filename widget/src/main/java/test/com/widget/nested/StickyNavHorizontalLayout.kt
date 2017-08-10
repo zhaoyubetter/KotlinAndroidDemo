f14704309b420069dd3addebac6dfc1b29984c93
better <zhaoyubetter@126.com>
2017-08-10 14:08:43 +0800

    水平nestd - 加入 【加速度】 与 【scroller】

--- widget/src/main/java/test/com/widget/nested/StickyNavHorizontalLayout.kt
+++ widget/src/main/java/test/com/widget/nested/StickyNavHorizontalLayout.kt
@@ -2,10 +2,13 @@ package test.com.widget.nested
 
 import android.content.Context
 import android.util.AttributeSet
+import android.util.Log
 import android.view.MotionEvent
+import android.view.VelocityTracker
 import android.view.View
 import android.view.ViewConfiguration
 import android.widget.LinearLayout
+import android.widget.Scroller
 import test.com.widget.R
 
 /**
@@ -23,17 +26,24 @@ class StickyNavHorizontalLayout(context: Context, attrs: AttributeSet?, defAttrS
     private var scrollView: View? = null
 
     // --- 操作相关
-    private var topViewHeight: Int = 0
+    private var topViewWidth: Int = 0
     private var lastX: Float = 0f
     private var isDrag = false      // 是否拖拽
+    private var velocityTracker: VelocityTracker? = null
+    private var scroller: Scroller
 
     // --- 配置相关
     private var touchSlop: Int = 0
+    private var maxVelocity: Int = 0
+    private var minVelocity: Int = 0
 
     init {
         orientation = HORIZONTAL
         val config = ViewConfiguration.get(context)
         touchSlop = config.scaledTouchSlop
+        maxVelocity = config.scaledMaximumFlingVelocity
+        minVelocity = config.scaledMinimumFlingVelocity
+        scroller = Scroller(context)
     }
 
     override fun onFinishInflate() {
@@ -46,20 +56,30 @@ class StickyNavHorizontalLayout(context: Context, attrs: AttributeSet?, defAttrS
     override fun onMeasure(widthMeasureSpec: Int, heightMeasureSpec: Int) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec)
         scrollView?.let {
-            scrollView?.layoutParams?.height = this@StickyNavHorizontalLayout.measuredHeight.minus(navView?.measuredHeight ?: 0)
+            scrollView?.layoutParams?.width = this@StickyNavHorizontalLayout.measuredWidth.minus(navView?.width ?: 0)
         }
     }
 
     override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
         super.onSizeChanged(w, h, oldw, oldh)
-        topViewHeight = topView?.measuredHeight ?: 0
+        topViewWidth = topView?.measuredWidth ?: 0
     }
 
     override fun onTouchEvent(event: MotionEvent?): Boolean {
         event?.let {
+            initVelocityTracker()
+            velocityTracker?.addMovement(event)
+
             val x = it.x
             when (it.action) {
-                MotionEvent.ACTION_DOWN -> lastX = x
+                MotionEvent.ACTION_DOWN -> {
+                    lastX = x
+                    if (!scroller.isFinished) {
+                        scroller.abortAnimation()
+                        return true
+                    }
+                }
+
                 MotionEvent.ACTION_MOVE -> {
                     val dx = x - lastX
                     if (!isDrag && Math.abs(dx) > touchSlop) isDrag = true
@@ -70,9 +90,19 @@ class StickyNavHorizontalLayout(context: Context, attrs: AttributeSet?, defAttrS
                 }
                 MotionEvent.ACTION_UP -> {
                     isDrag = false
+                    velocityTracker?.let {
+                        it.computeCurrentVelocity(1000, maxVelocity.toFloat())
+                        Log.e("better", "min: ${minVelocity}, current:${it.xVelocity}")
+                        if (Math.abs(it.xVelocity) > minVelocity) {  // 加速度
+                            flingX(-it.xVelocity)
+                        }
+                    }
+                    releaseVelocity()
                 }
                 MotionEvent.ACTION_CANCEL -> {
                     isDrag = false
+                    if(!scroller.isFinished)  scroller.abortAnimation()
+                    releaseVelocity()
                 }
             }
         }
@@ -86,4 +116,28 @@ class StickyNavHorizontalLayout(context: Context, attrs: AttributeSet?, defAttrS
         //return super.onInterceptTouchEvent(ev)
     }
 
+    override fun computeScroll() {
+        super.computeScroll()
+        if (scroller.computeScrollOffset()) {
+            scrollTo(scroller.currX, 0)
+            invalidate()
+        }
+    }
+
+    private inline fun flingX(velocityX: Float) {
+        // int startX, int startY, int velocityX, int velocityY, int minX, int maxX, int minY, int maxY
+        scroller.fling(scrollX, 0, velocityX.toInt(), 0, 0, topViewWidth, 0, 0)
+        invalidate()
+    }
+
+    private inline fun initVelocityTracker() {
+        if (velocityTracker == null) velocityTracker = VelocityTracker.obtain()
+    }
+
+    private inline fun releaseVelocity() {
+        velocityTracker?.let {
+            it.recycle()
+            velocityTracker = null
+        }
+    }
 }
