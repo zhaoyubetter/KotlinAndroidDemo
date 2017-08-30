beea5a8acfd3f6d14fd5cca5ede63ad234acdfd5
better <zhaoyubetter@126.com>
2017-08-30 14:30:25 +0800

    gradle task 的 outputs 与 inputs

--- widget/src/main/java/test/com/widget/ui/NestdScrollingTestActivity.kt
+++ widget/src/main/java/test/com/widget/ui/NestdScrollingTestActivity.kt
@@ -2,6 +2,8 @@ package test.com.widget.ui
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.widget__activity_nestd_scrolling_test.*
+import org.jetbrains.anko.sdk25.coroutines.onClick
 
 import test.com.widget.R
 
@@ -10,5 +12,9 @@ class NestdScrollingTestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.widget__activity_nestd_scrolling_test)
+        btn.onClick {
+//            it?.scrollBy(0, -100)     // 在自己的显示区域中动
+            it?.translationX = 100f     // 在父容器中動
+        }
     }
 }
